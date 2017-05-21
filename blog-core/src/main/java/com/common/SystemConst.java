a1116a01162a18ae26bdfba87d74678176798752
A-Trueman <601076628@qq.com>
2017-05-22 00:19:02 +0800

    登录验证，撰写博文

--- blog-core/src/main/java/com/common/SystemConst.java
+++ blog-core/src/main/java/com/common/SystemConst.java
@@ -7,6 +7,6 @@ public final class SystemConst {
     public static final String PATH = "E:\\";
     public static final String TARGETPath = "/wwwroot/www";
     public static final String HOST = "120.25.246.250";
-    public static final String USER = "root";
-    public static final String PASSWORD = "1995lcg!";
+    public static final String USER = "test";
+    public static final String PASSWORD = "test";
 }
